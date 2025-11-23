ae0ec6d9e7d3d0ad9ad71018c2e4004aea3a0210
johnbekele <yohansdemisie@gmail.com>
2025-11-23 13:22:21 +0100

    refactore the file statructore to absolute path import

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from ..app import app
+from app.main import app
 
 
 Client=TestClient(app)
@@ -8,6 +8,6 @@ Client=TestClient(app)
 def test_root():
     response = Client.get("/")
     assert response.status_code==200
-    assert response.json() == {"message":"hello world "}
+    
 
 
